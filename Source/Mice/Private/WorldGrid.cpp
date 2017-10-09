273c81544a2b515c3ee6cb7bb5e8209199dedf39
JoelWaterworth <joelwaterworth@gmail.com>
2017-10-09 13:43:18 +0100

    Added text and I have begun adding the grid collision

--- Source/Mice/Private/WorldGrid.cpp
+++ Source/Mice/Private/WorldGrid.cpp
@@ -12,16 +12,20 @@ AWorldGrid::AWorldGrid()
 	root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	InstanceMesh = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstanceMesh"));
 	InstanceMesh->SetupAttachment(root);
-	/*
+
+	width = 10;
+	height = 10;
+	spacing = 120;
+
 	for (int x = 0; x < width; x++)
 	{
 		for (int y = 0; y < height; y++)
 		{
-	//		InstanceMesh->Add();
-	//		USceneComponent* baseScene = NewObject<USceneComponent>(this, USceneComponent::StaticClass());
+			FTransform trans = FTransform(FVector((float)(x * spacing), (float)(y * spacing), 0.0f));
+			InstanceMesh->AddInstance(trans);
 		}
 	}
-	*/
+	
 }
 
 // Called when the game starts or when spawned
@@ -36,5 +40,31 @@ void AWorldGrid::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AWorldGrid::OnConstruction(const FTransform & Transform)
+{
+	for (auto& com : waste) {
+		com->DestroyComponent(true);
+		waste.Remove(com);
+	}
+
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			UTextRenderComponent* text = NewObject<UTextRenderComponent>(this, UTextRenderComponent::StaticClass());
+			FTransform textTrans = FTransform(FRotator(0.0f, 90.f, 0.0f), FVector((float)(width * spacing), (float)(height * spacing), 10.0f), FVector());
+			text->SetRelativeTransform(textTrans);
+			waste.Add(text);
+
+			UGridCollision* col = NewObject<UGridCollision>(this, UGridCollision::StaticClass());
+			FTransform colTrans = FTransform(FRotator(), FVector((float)(width * spacing), (float)(height * spacing), 0.0f), FVector());
+			col->SetRelativeTransform(colTrans);
+			waste.Add(col);
+			col->SetBoxExtent(CollisionExtent, true);
+			col->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Block)
+		}
+	}
 }
 
--- enc_temp_folder/7a68eccc4757aa34ad3e1359d441fcd2/WorldGrid.cpp
+++ enc_temp_folder/7a68eccc4757aa34ad3e1359d441fcd2/WorldGrid.cpp
@@ -13,15 +13,19 @@ AWorldGrid::AWorldGrid()
 	InstanceMesh = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstanceMesh"));
 	InstanceMesh->SetupAttachment(root);
 
+	width = 10;
+	height = 10;
+	spacing = 120;
+
 	for (int x = 0; x < width; x++)
 	{
 		for (int y = 0; y < height; y++)
 		{
-			InstanceMesh->Add();
-			USceneComponent* baseScene = NewObject<USceneComponent>(this, USceneComponent::StaticClass());
+			FTransform trans = FTransform(FVector((float)(x * spacing), (float)(y * spacing), 0.0f));
+			InstanceMesh->AddInstance(trans);
 		}
 	}
-
+	
 }
 
 // Called when the game starts or when spawned
@@ -36,5 +40,25 @@ void AWorldGrid::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AWorldGrid::OnConstruction(const FTransform & Transform)
+{
+	for (auto& com : waste) {
+		com->DestroyComponent(true);
+		waste.Remove(com);
+	}
+
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			UTextRenderComponent* text = NewObject<UTextRenderComponent>(this, UTextRenderComponent::StaticClass());
+			FTransform trans = FTransform(FRotator(0.0f, 90.f, 0.0f), FVector((float)(width * spacing), (float)(height * spacing), 10.0f), FVector());
+			
+			text->SetRelativeTransform(trans);
+			waste.Add(text);
+		}
+	}
 }
 
