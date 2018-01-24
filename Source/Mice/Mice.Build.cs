5b2b7a2ba4ab532c478e0c60ac4b272c23cc3139
JoelWaterworth <joelwaterworth@gmail.com>
2018-01-24 09:01:04 +0000

    fog of war compiles now compiles

--- Source/Mice/Mice.Build.cs
+++ Source/Mice/Mice.Build.cs
@@ -8,7 +8,7 @@ public class Mice : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "RHI", "RenderCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
